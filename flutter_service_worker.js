21e196e46965c3bc3d21cdecfe0676a17b635e4b
IvanRuiz23 <ivanruiz4964@gmail.com>
2023-03-21 13:53:07 -0600

    Error de HTTPS.
    Signed-off-by: IvanRuiz23 <ivanruiz4964@gmail.com>

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -42,9 +42,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "10d22afae13d4bce01c068aacdde40a5",
-"/": "10d22afae13d4bce01c068aacdde40a5",
-"main.dart.js": "7bf7c605c53affdd7261f573281d5667",
+"index.html": "ede8001090bfba610ebf8357bdfd54a3",
+"/": "ede8001090bfba610ebf8357bdfd54a3",
+"main.dart.js": "7a2a09ae1a65260c4d7a334a821b63d9",
 "manifest.json": "5a9e9197ff0495b36de191f960ee6fda",
 "version.json": "6ce9bbeeefe2e4883b3ffb864995c282"
 };
